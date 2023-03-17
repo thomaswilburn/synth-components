f1d9603523440773d0a1e5a741b94a317f1fa84a
Thomas Wilburn <news@thomaswilburn.net>
2023-03-17 00:38:38 -0500

    meh

--- src/fx-distortion.js
+++ src/fx-distortion.js
@@ -2,23 +2,34 @@ import context from "./lib/audio-context.js";
 import { BaseTile } from "./lib/base-tile.js";
 
 const PI = Math.PI;
-const POINTS = 7;
+const POINTS = 15;
+
+const CURVES = {
+  k: (a, x) => (PI + a * 100) * x / (PI + a * 100 * Math.abs(x)),
+  e: (a, x) => (1 - Math.E ** (-Math.abs(x))) || 0,
+  sin: (a, x) => Math.sin(x * PI * .5) * (a * .5 + 1),
+  atan: (a, x) => (Math.atan(x) / PI) * (4 + a * 2),
+  hard: (a, x) => x * (a + 1)
+}
 
 export class FxDistortion extends BaseTile {
+
+  type = "k";
+
   constructor() {
     super();
 
     this.audioNode = new WaveShaperNode(context);
-    this.audioNode.curve = this.generateCosineCurve();
-    this.audioNode.oversample = true;
+    this.audioNode.curve = this.generateCurve();
+    this.audioNode.oversample = "4x";
   }
 
-  generateCosineCurve(overdrive = .5) {
+  generateCurve(overdrive = .5) {
     var curve = new Float32Array(POINTS);
-    var k = overdrive * 100;
+    var shaper = CURVES[this.type];
     for (var i = 0; i < curve.length; i++) {
       var x = (i / (POINTS - 1)) * 2 - 1;
-      curve[i] = (PI + k) * x / (PI + k * Math.abs(x));
+      curve[i] = shaper(overdrive, x);
     }
     return curve;
   }
@@ -28,7 +39,7 @@ export class FxDistortion extends BaseTile {
     super.attributeChangedCallback(attr, was, value);
     switch (attr) {
       case "drive":
-        this.audioNode.curve = this.generateCosineCurve(value * 1);
+        this.audioNode.curve = this.generateCurve(value * 1);
         break;
     }
   }
